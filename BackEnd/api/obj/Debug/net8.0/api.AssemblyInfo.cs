88cc724c67c2641868991a87398eb43f2470287f
0Rohit2022 <rohitkumarrr2022@gmail.com>
2024-09-26 00:59:34 +0530

    Created Model for the Stock and Comment

--- BackEnd/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ BackEnd/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d677d0a94eaadb33525b1b3ffb43eade0f3793")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
