ee2552d0700cced0a4633c33203e44d97a580099
Rohit kumar <rohitkumarrr2022@gmail.com>
2024-11-01 19:36:56 +0530

    Added pagination inside the Getall api for the stocks and with the help of skip and take method which is already in the .net

--- BackEnd/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ BackEnd/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd263ddcad4fadc1649de3ebe559929e264883c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31c5578e89414ace4f6fb0bd6b96bb8105a67a2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
